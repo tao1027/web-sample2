817dacadee75208dae18797cfd44d4eebedeface
tao <s13218to@sfc.keio.ac.jp>
2014-12-22 04:40:06 +0900

    HPでおすすめのお店は未完成上はだいたい完成

---
+++ sample.php
@@ -0,0 +1,99 @@
+<html>
+
+<head>
+
+<title>新年会をしよう！</title>
+
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+
+<body>
+<body background="back.png">
+
+<div id="site-box">
+
+	<div id = "site-box-had">
+	</div>
+
+	<div id = "site-box-side-a">
+		</br>
+	</div>
+
+	<div id = "site-box-left"　Div Align="left">
+		<img src="./cover.jpg"　alt = "cover">
+	</div>
+
+	<div id = "site-box-side-b">
+		</br>
+	</div>
+
+	<div id = "site-box-right">
+		〜新年会〜</br>
+    	NECO.Lab 2015
+	</div>
+
+	<div id = "site-box-side-c">
+		</br>
+	</div>
+	<div style="clear: both"></div>
+
+</div>
+</br>
+<div id = "osusume-box">
+	<!--<div id = "osusumetitle-box" div align="left">
+		おすすめのお店
+	</div>-->
+
+	<div id = "osusume-side-a">
+		</br>
+	</div>
+
+	<div id = "ab-box">
+		
+		<div id="a-box">
+			押見担当</br>
+			<?php
+			require("osusume-a.html")
+			?>
+		</div>
+
+		<div id="b-box">
+			武田担当</br>
+			<?php
+			require("osusume-a.html")
+			?>
+		</div>
+
+	</div>
+
+	<div id = "osusume-side-b">
+		</br>
+	</div>
+
+	<div id = "cd-box">	
+		
+		<div id="c-box">
+			千葉担当</br>
+			<?php
+			require("osusume-a.html")
+			?>
+		</div>
+
+		<div id="d-box">
+			南雲担当</br>
+			<?php
+			require("osusume-a.html")
+			?>
+		</div>
+
+	</div>
+	<div id = "osusume-side-c">
+		</br>
+	</div>
+
+</div>
+ 
+</body> 
+
+</html>
